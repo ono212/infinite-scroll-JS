e8e76442132804071847bcb5879999e597514bac
jungmin <qkrwjdals302@naver.com>
2022-06-18 13:55:23 +0900

    Chore: 초기 세팅 - webpack, loader, dotenv 설치

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./src/index.html" }),
+    new Dotenv(),
+    new CleanWebpackPlugin(),
+  ],
+};
