b0c5abbbdeffbf245a3d713ae98cae9f19cc0ed9
jungmin <qkrwjdals302@naver.com>
2022-06-18 19:37:41 +0900

    Feat: 무한 스크롤 기능 구현 - IntersectionObserver 이용

--- src/PhotoList.js
+++ src/PhotoList.js
@@ -11,8 +11,28 @@ export default function PhotoList({ $target, initialState, onScrollEnded }) {
     this.render();
   };
 
+  const callback = (entries, observer) => {
+    entries.forEach((entry) => {
+      /*
+      li가 겹쳐졌을 때, 그리고 isLoaing이 false일 때 Api를 요청한다.
+      이미 isLoading === true인 상황에서 또 Api가 요청되는 경우를 막기 위함이다.
+      */
+      if (entry.isIntersecting && !this.state.isLoading) {
+        // Api 요청
+        onScrollEnded();
+
+        // 기존에 등록된 li요소는 해제해야한다. 해제하지않으면 스크롤을 다시 올렸을 때 예전에 등록한 li를 탐지한다.
+        observer.unobserve(entry.target);
+      }
+    });
+  };
+
+  const observer = new IntersectionObserver(callback, {
+    threshold: 0.5, // 마지막 이미지의 절반이 걸쳐졌을 때를 탐지한다!
+  });
+
   this.render = () => {
-    // 첫 렌더링 때에만 ul 생성
+    // 첫 렌더링 때에만 ul 생성하도록 isInitialized 변수를 사용한다.
     if (!isInitialized) {
       $photoList.innerHTML = `
           <ul class="PhotoList_photos"></ul>
@@ -32,6 +52,11 @@ export default function PhotoList({ $target, initialState, onScrollEnded }) {
         $photosUl.appendChild($li);
       }
     });
+
+    const $nextLi = $photosUl.querySelector("li:last-child");
+
+    // 매번, 불러온 이미지들 중 마지막 이미지에 observer을 등록한다.
+    if ($nextLi !== null) observer.observe($nextLi);
   };
 
   this.render();
